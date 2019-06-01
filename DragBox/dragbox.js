4c0cde78ca8ae1f9a1d3f82e628919cd04c83868
Jason Agosta <agostaj@d20green.com>
2019-06-01 12:45:31 -0400

    Using Application.Dragging now

--- DragBox/dragbox.js
+++ DragBox/dragbox.js
@@ -9,24 +9,17 @@ Application = {
 	load: function(e) {
 		console.log("LOaDED");
 		this.bigbox = document.getElementById('bigbox');
-		console.log(this.bigbox);
 		this.blank = document.getElementById('blank');
 	},
 };
 
 Application.Dragging = new (class Dragging {
-	// constructor() {
-	// 	this.foo = 77;
-	// }
-
 	dragStart(ev) {
+		let box_width = parseInt(ev.target.offsetWidth/2) + "px";
+		let box_height = parseInt(ev.target.offsetHeight/2) + "px";
+
 		ev.dataTransfer.effectAllowed='move';
-		ev.dataTransfer.setDragImage(img, BOX_SIZE + "px", BOX_SIZE + "px");
-
-		greeny = ev.target;
-		greeny.Class = new DragBox();
-
-		console.log(greeny);
+		ev.dataTransfer.setDragImage(Application.blank, box_width, box_height);
 	}
 
 	dragEnd(ev) {
@@ -34,7 +27,9 @@ Application.Dragging = new (class Dragging {
 	}
 
 	drag(ev) {
-		// bigbox is global now.
+		let bigbox = Application.bigbox;
+		let BOX_SIZE = Application.BOX_SIZE;
+
 		var dragbox = ev.target;
 		var posX = (ev.x - BOX_SIZE);
 		var posY = (ev.y - BOX_SIZE);
@@ -47,7 +42,6 @@ Application.Dragging = new (class Dragging {
 		var maxY = bigbox.offsetHeight/2 - dragbox.offsetHeight/2;
 		var minY = -bigbox.offsetHeight/2 + dragbox.offsetHeight/2;
 
-		// // These are not quite in the correct coordinate space.
 		if(offCenterPosX > maxX) {
 			let border_right = getComputedStyle(dragbox).getPropertyValue('border-right-width');
 			posX = bigbox.offsetLeft + bigbox.offsetWidth - BOX_SIZE * 2 - parseInt(border_right);
@@ -69,6 +63,11 @@ Application.Dragging = new (class Dragging {
 		ev.target.innerHTML = offCenterPosX + ", " + offCenterPosY;
 	}
 
+	correctForCollisions(ev) {
+		let bigbox = Application.bigbox;
+
+	}
+
 	dragOver(ev) {
 		ev.preventDefault();
 		return true;
@@ -81,86 +80,16 @@ Application.Dragging = new (class Dragging {
 	}
 })();
 
-Application.Classes.DragBox = class DragBox {
-	constructor() {
-		this.foo = 88;
+// Application.Classes.DragBox = class DragBox {
+// 	constructor() {
+// 		this.foo = 88;
 
-		let el = document.createElement("DIV");
-		console.log(el);
+// 		let el = document.createElement("DIV");
+// 		console.log(el);
 
-		return el;
-	}
-}
-
-Application.BOX_OFFSET = Application.BOX_SIZE/2;
-var BOX_SIZE = Application.BOX_SIZE;
-var bigbox;
-var img;
-
-
-function dragStart(ev) {
-	bigbox = document.getElementById('bigbox');
-	img = document.getElementById('blank');
-
-	ev.dataTransfer.effectAllowed='move';
-	ev.dataTransfer.setDragImage(img, BOX_SIZE + "px", BOX_SIZE + "px");
-
-	greeny = ev.target;
-	greeny.Class = new Application.Classes.DragBox();
-
-	console.log(greeny);
-}
-
-function dragEnd(ev) {
-	// Empty
-}
-
-function drag(ev) {
-	// bigbox is global now.
-	var dragbox = ev.target;
-	var posX = (ev.x - BOX_SIZE);
-	var posY = (ev.y - BOX_SIZE);
-
-	var offCenterPosX = ev.x - bigbox.offsetLeft - bigbox.offsetWidth/2;
-	var offCenterPosY = ev.y - bigbox.offsetTop - bigbox.offsetHeight/2;
-
-	var maxX = bigbox.offsetWidth/2 - dragbox.offsetWidth/2;
-	var minX = -bigbox.offsetWidth/2 + dragbox.offsetWidth/2;
-	var maxY = bigbox.offsetHeight/2 - dragbox.offsetHeight/2;
-	var minY = -bigbox.offsetHeight/2 + dragbox.offsetHeight/2;
-
-	// // These are not quite in the correct coordinate space.
-	if(offCenterPosX > maxX) {
-		let border_right = getComputedStyle(dragbox).getPropertyValue('border-right-width');
-		posX = bigbox.offsetLeft + bigbox.offsetWidth - BOX_SIZE * 2 - parseInt(border_right);
-	} else if(offCenterPosX < minX) {
-		posX = bigbox.offsetLeft;
-	}
-
-	if(offCenterPosY > maxY) {
-		let border_left = getComputedStyle(dragbox).getPropertyValue('border-left-width');
-		posY = bigbox.offsetTop + bigbox.offsetHeight - BOX_SIZE * 2 - parseInt(border_left);
-	} else if(offCenterPosY < minY) {
-		posY = bigbox.offsetTop;
-	}
-
-	dragbox.style.position = "absolute";
-	dragbox.style.left = posX+'px';
-	dragbox.style.top = posY+'px';
-
-	ev.target.innerHTML = offCenterPosX + ", " + offCenterPosY;
-}
-
-function dragOver(ev) {
-	ev.preventDefault();
-	return true;
-}
-
-
-function dragDrop(ev) {
-	ev.stopPropagation();
-	return false;
-}
+// 		return el;
+// 	}
+// }
 
 
 addEventListener('load', Application.load());
